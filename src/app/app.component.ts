de554316d7b5c8a99651ee6d05d73e723629611e
Heccubernny <ipom4eva@gmail.com>
2020-10-07 13:51:36 +0100

    updating code and tutorial finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,85 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  name = 'angcorreg';
+  ServerName = "Express";
+  ServerPID = 11;
+  ServerStatus = 'offline';
+  StatusFlag = false;
+  buttonState = true;
+  disableButton = '';
+  state = false;
+  flag=true;
+  tdstate = true;
+  counter = [];
+
+  studentRoster = ['Arya', 'Rohit', 'Upasana'];
+  currentName = '';
+  
+
+  toggleDisplay(){
+    this.tdstate = !this.tdstate;
+    this.counter.push(this.counter.length + 1);
+  }
+
+  getlength(){
+    if (this.counter.length > 4) {
+      return 'blue';
+      
+    }else{
+      return 'black';
+    }
+  }
+
+
+  add(){
+    this.studentRoster.push(this.currentName);
+  }
+
+  toggleFlag(){
+    this.flag = !this.flag;
+    return this.flag;
+  }
+
+  resetServerStatus(){
+    this.ServerName='';
+  }
+ 
+  getColor(){
+    if (this.flag === true) {
+      return "green";
+    }else{
+      return 'red';
+    }
+  }
+
+  checkName(){
+    if(this.ServerName === ''){
+      this.state = true;
+      return this.state;
+    }
+  }
+
+
+  constructor() {
+    setTimeout(() => {
+      this.buttonState = false;
+    }, 2500);
+    
+  }
+  
+
+  toggleServerStatus() {
+    this.StatusFlag = !this.StatusFlag;
+
+    if (this.StatusFlag === true) {
+      this.ServerStatus = 'online';
+    }
+    else{
+      this.ServerStatus = "offline";
+    }
+
+    return this.ServerStatus;
+  }
+
+
 }
